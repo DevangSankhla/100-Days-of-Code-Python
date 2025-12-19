f396d01d5312273187e729324e2d0806c5d9a47d
DevangSankhla <artisandevang1234@gmail.com>
2025-12-19 15:38:58 +0530

    Day 7: Hangman: Game

---
+++ Day-07-Hangman/main.py
@@ -0,0 +1,50 @@
+import random
+from hangman_words import word_list
+from hangman_art import stages
+from hangman_art import logo
+
+print(logo)
+lives = 6
+chosen_word = random.choice(word_list)
+#print(chosen_word)
+
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print("Word to guess: " + placeholder)
+
+game_over = False
+correct_letters = []
+guesslist = []
+while not game_over:
+    print(f"**************************** {lives} LIVES LEFT ****************************")
+    guess = input("Guess a letter: ").lower()
+    if guess in guesslist:
+        print("You have already guessed this letter, " + guess + '.')
+    else:
+        print()
+
+    display = ""
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+    print("Word to guess: " + display)
+
+    if guess not in chosen_word:
+        lives -= 1
+        print("You guessed " + guess + ", that's not in the word. You lose a life.")
+        if lives == 0:
+            game_over = True
+            print(f"***********************YOU LOSE**********************\nThe correct word was {chosen_word}.")
+
+    if "_" not in display:
+        game_over = True
+        print("****************************YOU WIN****************************")
+    print(stages[lives])
+    guesslist.append(guess)
